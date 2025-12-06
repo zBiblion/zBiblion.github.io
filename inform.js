2335246ee076720b9c25c20ed115cfcbe94732d2
bookforever <book.bcc58@gmail.com>
2025-12-06 16:47:05 +0700

    y

--- inform.js
+++ inform.js
@@ -1,1 +1,8 @@
-let 
+const button = document.getElementById('myButton');
+const messageArea = document.getElementById('messageArea');
+
+// Add a 'click' event listener to the button
+button.addEventListener('click', () => {
+    // When the button is clicked, change the message area's text
+    messageArea.textContent = 'byebye';
+})
